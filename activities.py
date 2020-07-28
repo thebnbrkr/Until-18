11ff11d02cc18fa60253cde6d117d3ff1afc4ebe
thebnbrkr <51245304+thebnbrkr@users.noreply.github.com>
2020-07-28 13:04:44 +0530

    Add files via upload

---
+++ activities.py
@@ -0,0 +1,10 @@
+Boys=["Liam","Noah","William","James","Oliver","Benjamin","Elijah","Lucas","Mason","Logan","Anderson","Blakely","Bo","Bodie","Boden","Brit","Lotte","Gioto","Xander","Loius","Paolo","Marcelo"
+
+]
+Girls=["Emma","Olivia","Ava","Isabella","Sophia","Charlotte","Mia","Amelia","Harper","Evelyn","Alice","Maja","Elsa","Ella","Ebba","Alica","Olivia","Abella","Mia","Isla","Maya","Michelle",
+
+"Adriana","Lola","Clare"]
+
+LastNames=["Kim","lee","park","mae","choi","chen","wang","yang","Smith","Johnson","William","Brown","Rodriguez","Martinez","Davis","Garzia","Jones","Hall"]
+
+
